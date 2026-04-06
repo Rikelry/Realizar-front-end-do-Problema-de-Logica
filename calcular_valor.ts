c4295912aee086cb4b56ea56eece4d09ee4c80f8
Rikelry <153693133+Rikelry@users.noreply.github.com>
2026-04-06 16:18:52 -0300

    Rename Breakdown type to Detalhamento

--- calcular_valor.ts
+++ calcular_valor.ts
@@ -1,9 +1,9 @@
-type Breakdown = {
+type Detalhamento = {
     notas: Record<number, number>;
     moedas: Record<number, number>;
 }
 
-export function calcular_valor(valor: number): Breakdown {
+export function calcular_valor(valor: number): Detalhamento {
     let centavos = Math.round(valor * 100);
     const valor_notas = [10000, 5000, 2000, 1000, 500, 200];
     const valor_moedas = [100, 50, 25, 10, 5, 1];
